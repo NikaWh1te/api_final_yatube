61c39578d38e1ead0eb4d164f761cda1fbf9c352
NikaWh1te <108050036+NikaWh1te@users.noreply.github.com>
2025-03-23 21:13:22 +0700

    Update permissions.py

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework import permissions
 
 
-class OwnershipPermission(permissions.BasePermission):
+class IsAuthenticatedOrOwner(permissions.BasePermission):
 
     def has_permission(self, request, view):
         return (
