474c708eefa852dd1f44cac35dca1066e5775feb
NikaWh1te <108050036+NikaWh1te@users.noreply.github.com>
2025-03-23 21:21:46 +0700

    Update permissions.py
    Избыточная проверка

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -4,10 +4,7 @@ from rest_framework import permissions
 class IsAuthenticatedOrOwner(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+        return True  # Разрешаем доступ на уровень вьюсета, объектные права проверяются отдельно
 
     def has_object_permission(self, request, view, obj):
         return (
